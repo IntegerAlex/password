89704e9ea879ec91ab527ac58f95f5d9f35d94e4
Akshat Kotpalliwar <inquiry.akshatkotpalliwar@gmail.com>
2023-09-19 01:39:27 +0530

    optimised input boxes

--- X-security/src/App.jsx
+++ X-security/src/App.jsx
@@ -1,13 +1,40 @@
-// import { useState } from 'react'
+ import { useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const [site, setSite] = useState(null)
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  function handleSiteChange() {
+    const Site = document.getElementById('site').value
+    const user = document.getElementById('user').value
+    const pass = document.getElementById('pass').value
+
+
+    setSite(Site)
+    setUsername(user)
+    setPassword(pass)
+    
+    if (site === null || username === null || password === null) {
+      alert('Please enter all fields')
+    }
+    else {
+    console.log(site)
+    console.log(username)
+    }
+  }
+
+
 
 
   return (
     <>
-    
+      <input id="site" type="text" placeholder="Enter site" />
+      <input id="user" type="text" placeholder="Enter username" />
+      <input id="pass" type="text" placeholder="Enter password" />
+      <button onClick={handleSiteChange}>SAVE</button>
     </>
   )
 }
